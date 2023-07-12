2795a75f95d353d603767fdde9994cd9c593a0f0
Nelson Galdeman <nelsongaldeman@gmail.com>
2023-07-12 16:36:34 +0100

    fix: updates to USDT + gets decimals from contract

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ require('dotenv').config();
 let privateKey = process.env.PRIVATE_KEY; // replace with your private key
 let providerUrl = process.env.RPC_URL; // replace with your Infura project ID or any Ethereum node RPC URL
 
-// DAI
-let contractAddress = '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063'; // replace with your contract's address
+// USDT
+let contractAddress = '0xc2132d05d31c914a87c6611c10748aeb04b58e8f'; // replace with your contract's address
 
 let functionSig = '0xa9059cbb';
 let recipientAddress = '0x9d88718Fa25daFa0544799C79EE43b752b08B2A2';
-let amount = 1; //in wei
+let amount = 0.0001;
 let arbitraryData = stringToHex('Hola nico, esto funciona!!');
 
 let wallet, provider, transaction, signedTx, txResponse;
@@ -21,6 +21,13 @@ async function sendTx() {
 
   // Initialize a wallet with the private key and connect it to the provider
   wallet = new ethers.Wallet(privateKey, provider);
+
+  // Initialize a contract instance
+  const contract = new ethers.Contract(contractAddress, ['function decimals() public view returns (uint8)'], provider);
+
+  // Get the number of decimals for the token
+  const decimals = await contract.decimals();
+
   // Remove '0x' from the functionSig
   functionSig = functionSig.slice(2);
 
@@ -28,7 +35,7 @@ async function sendTx() {
   recipientAddress = recipientAddress.slice(2).padStart(64, '0');
 
   // Convert the amount to hex and pad it
-  amount = ethers.utils.hexZeroPad(ethers.utils.hexlify(amount), 32).slice(2);
+  amount = ethers.utils.hexZeroPad(ethers.utils.parseUnits(amount.toString(), decimals), 32).slice(2);
 
   // Construct the transaction data
   let data = '0x' + functionSig + recipientAddress + amount + arbitraryData;
